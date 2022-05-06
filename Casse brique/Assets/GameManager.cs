7c944906644a5a8a83549aa03b86c1536ea69e56
Droledami <justinmichaux1993@gmail.com>
2022-05-06 17:27:46 +0200

    6 main + gitignore update
    Avec de l'espoir, le gitignore empêchera les erreurs au démarrage ou on doit supprimer des fichiers des Logs

---
+++ Casse brique/Assets/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Barre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Barre = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
