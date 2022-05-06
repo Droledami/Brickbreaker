c784f581bd15c3436bc77f78da1fc5f967c35e16
Nicksow <nick93736@gmail.com>
2022-05-06 16:04:20 +0200

    2e commit

---
+++ Casse brique/Assets/BarreScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarreScript : MonoBehaviour
+{
+    public float speed;
+    public float MurGauche;
+    public float MurDroit;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector2.right * horizontal * Time.deltaTime * speed);
+
+        if (transform.position.x < MurGauche)
+        {
+            transform.position = new Vector2(MurGauche, transform.position.y);
+        }
+        if (transform.position.x > MurDroit)
+        {
+            transform.position = new Vector2(MurDroit, transform.position.y);
+        }
+    }
+}
